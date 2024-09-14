87c7ae08daf9c3eeaba96d3047bd384c02e3a5e3
= <=>
2024-09-14 19:10:40 +0100

    Sep 14 Part 2

--- src/potd/y_2024/sep/sep_14.rs
+++ src/potd/y_2024/sep/sep_14.rs
@@ -14,3 +14,32 @@ impl Solution {
             .1
     }
 }
+
+impl Solution {
+    pub fn string_hash_2(s: String, k: i32) -> String {
+        (0..(s.len() / k as usize))
+            .map(|idx| {
+                ((0..k)
+                    .zip(s.as_bytes().iter().skip(idx * k as usize))
+                    .fold(0 as u8, |sum, (_i, &ch)| {
+                        (sum as u8 % 26 + (ch - 'a' as u8) % 26) % 26 as u8
+                    })
+                    + 'a' as u8) as char
+            })
+            .collect::<String>()
+    }
+}
+
+impl Solution {
+    pub fn string_hash(s: String, k: i32) -> String {
+        s.as_bytes()
+            .chunks(k as usize)
+            .map(|chunck| {
+                (chunck
+                    .iter()
+                    .fold(0 as u8, |sum, &ch| (sum % 26 + (ch - 'a' as u8) % 26) % 26)
+                    + 'a' as u8) as char
+            })
+            .collect::<String>()
+    }
+}
